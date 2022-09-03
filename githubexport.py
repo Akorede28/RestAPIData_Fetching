60e612c0f1398d973f7acdade757ce0beddd7c8b
Akorede28 <daniel.alamutu@gmail.com>
2022-09-03 11:41:07 +0100

    Added retry library and PySpark

--- githubexport.py
+++ githubexport.py
@@ -1,19 +1,44 @@
 from distutils.command.config import config
+from random import random
+from urllib import response
 import requests
 import json
 import os
+# import csv
+import time
 from pathlib import Path
 from dotenv import load_dotenv
+from pyspark.sql import *
+from transforms import transform
+# from tenacity import retry
+import retry
+
+# spark = SparkSession.builder.getOrCreate()
+# spark.sparkContext.setLogLevel("WARN")
+
 
 load_dotenv('.env')
 parent = Path(__file__).parent
 file_path = parent / 'github.json'
 
-class NotFoundException(Exception):
-    def __init__(self, message, code):
-        self._message = message
-        self._code = code
-        super().__init__(message, code)
+def retry(func, retries=3):
+    def retry_wrapper(*args, **kwargs):
+        attempts = 0
+        while attempts < retries:
+            try:
+                return func(*args, **kwargs)
+            except requests.exceptions.RequestException as e:
+                print(e)
+                time.sleep(2)
+                attempts += 1
+                print(attempts, "DONE")
+    return retry_wrapper
+
+# class NotFoundException(Exception):
+#     def __init__(self, message, code):
+#         self._message = message
+#         self._code = code
+#         super().__init__(message, code)
 
 
 class Config:
@@ -27,40 +52,40 @@ class Config:
 
 
 class GithubExportService:
-
+    @retry
     def get_commit_request(self, commit_url):
 
         response = requests.get(commit_url)
 
-        if(str(response.status_code).startswith('4')):
-            i = 0
+        # if(str(response.status_code).startswith('4')):
+        #     i = 0
 
-            while i <= 3:
-                response = requests.get(commit_url)
-                i += 1
-                print(i, "commit")
-                break
-            raise NotFoundException('no repo found', 404)      
+        #     while i <= 3:
+        #         response = requests.get(commit_url)
+        #         i += 1
+        #         print(i, "commit")
+        #         break
+        #     # raise NotFoundException('no repo found', 404)      
         print(response.json())
         return response
-
+    
+    @retry
     def get_pull_request(self, pull_url):
         
         response = requests.get(pull_url)
 
-        if(str(response.status_code).startswith('4')):
-            i = 0
+        # if(str(response.status_code).startswith('4')):
+        #     i = 0
 
-            while i <= 3:
-                response = requests.get(pull_url)
-                i += 1
-                print(i, "pull")
-                break
-            raise NotFoundException('no repo found', 404)
+        #     while i <= 3:
+        #         response = requests.get(pull_url)
+        #         i += 1
+        #         print(i, "pull")
+        #         break
+        #     raise NotFoundException('no repo found', 404)
         print(response.json())
         return response
-
-    
+   
     def make_file(self, response):
           if file_path.exists:
             with open(file_path, 'w') as f:
@@ -69,7 +94,7 @@ class GithubExportService:
             with open(file_path, 'x') as f:
                     json.dump(response.json(), f)
 
-
+# transform()
 
 github = GithubExportService()
 config = Config()
@@ -77,6 +102,3 @@ config = Config()
 res = github.get_commit_request(config.commit_url)
 pull = github.get_pull_request(config.pull_url)
 github.make_file(res)
-
-
-
